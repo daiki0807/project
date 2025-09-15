2f4e43edf64d8e09f1a83c5e967b29dee2cb0c8e
daiki <d.a0807derude@gmail.com>
2025-09-15 22:13:20 +0900

    feat: Add deployment workflow

--- 作戦ボード/vite.config.ts
+++ 作戦ボード/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/project/',
+  base: '/project/', // この行を追加
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
