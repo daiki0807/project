989593b9104dd90a0d1e6c143265d6d72971576a
daiki0807 <108675638+daiki0807@users.noreply.github.com>
2025-09-18 20:28:37 +0900

    Delete 作戦ボード directory

--- 作戦ボード/src/components/Fields/Court.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Court: React.FC = () => {
-  return (
-    <div className="w-full h-full relative bg-white rounded-lg">
-      <div className="absolute inset-4 border-4 border-black rounded-lg" />
-    </div>
-  );
-};
--- 作戦ボード/src/components/Fields/index.ts
+++
@@ -1,8 +0,0 @@
-export { SoccerField } from './SoccerField';
-export { BasketballCourt } from './BasketballCourt';
-export { VolleyballCourt } from './VolleyballCourt';
-export { BaseballField } from './BaseballField';
-export { Court } from './Court';
-export { Court2 } from './Court2';
-export { Basketball2Court } from './Basketball2Court';
-export { Basketball3Court } from './Basketball3Court';
